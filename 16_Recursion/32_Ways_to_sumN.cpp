0e5857baed8c190fab9fee610cdc16f31191248d
Harshvardhan6367 <harsh.work6367@gmail.com>
2025-04-08 23:17:12 +0530

    08/04/2025

---
+++ 16_Recursion/32_Ways_to_sumN.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int ways(int arr[],int m, int sum)
+{
+    if(sum==0)
+  return 1;
+  if(sum<0)
+  return 0;
+  
+  int ans = 0;
+  for(int i=0;i<m;i++)
+  {
+      ans+=ways(arr,m,sum-arr[i]);
+  }
+  return ans;
+}
+int main()
+{
+    int arr[]={1,5,6};
+    int sum = 7, m=3;
+    
+    cout<< ways(arr,m,sum);
+    
+}
